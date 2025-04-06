79149717dff0a83cae018153581a24357ee22488
Sarnendu Ghosh <154461219+Sarnendu74@users.noreply.github.com>
2025-04-06 11:33:22 +0530

    2D Array

---
+++ HourGlass.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[6][6];
+
+        // Input the 6x6 array
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        // Loop over all possible hourglass centers (i,j) such that a full hourglass can fit
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                int sum = arr[i][j]   + arr[i][j+1]   + arr[i][j+2]
+                                    + arr[i+1][j+1] +
+                          arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+                max = Math.max(max, sum);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
